9542eabafc0707776d199859f2dfa5905fbe74bf
Sergio Hilgert <hilgertfacul@gmail.com>
2017-02-10 08:07:00 +0000

    Add friends views and user-friends association

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => {:registrations => "user/registrations"}
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
+  
   root 'welcome#index'
   get 'my_portfolio' => 'users#my_portfolio'
   get 'search_stocks' => 'stocks#search'
   get 'my_friends' => 'users#my_friends'
+  get 'search_friends' => 'users#search'
+  post 'add_friend' => 'users#add_friend'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
